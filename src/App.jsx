21324b468d05ad78ae6567a09cd926b0160373b8
Othniel <chukchi2345@gmail.com>
2025-03-11 19:36:51 +0100

    Modified profile section, added name, button and resized image

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Bio  from "./component/bio"
+import Bio from "./component/bio";
 import firstImage from "./assets/drawers.jpg";
 
 function App() {
@@ -8,20 +8,21 @@ function App() {
 
   return (
     <>
-      <main className="w-[20rem]">
-        <img src={firstImage} alt="" />
-        <div>
-          <p className="font-bold">            
-              Shift the overall look and feel by adding these wonderful touches
-              to furniture in your home
+      <main className="main w-[20rem] h-[35rem]">
+        <div className="w-[100%] h-[38%] overflow-clip">
+          <img src={firstImage} alt="" />
+        </div>
+        <div className="flex flex-col justify-between items-center h-[62%] p-7 bg-gray-500">
+          <p className="font-bold">
+            Shift the overall look and feel by adding these wonderful touches to
+            furniture in your home
           </p>
-          <p>
+          <p className="">
             Ever been in a room and felt like something was missing? Perhaps it
             felt slightly bare and uninviting. I’ve got some simple tips to help
             you make any room feel complete
           </p>
-
-          < Bio />
+          <Bio />
         </div>
       </main>
     </>
--- src/component/bio.jsx
+++ src/component/bio.jsx
@@ -1,15 +1,19 @@
 import profilePicture from "../assets/avatar-michelle.jpg";
+import shareButton from "../assets/icon-share.svg";
 
 const Bio = () => {
+    console.log("Summer")
   return (
     <>
-      <section>
-        <img src={profilePicture} alt="" />
+      <section className="flex items-center">
+        <img className="w-[3rem] rounded-[2rem]" src={profilePicture} alt="" />
         <div>
-            <p>heybkfv</p>
-            <p></p>
+          <p>Michelle Appleton</p>
+          <p>28 Jun 2020</p>
         </div>
-        <button></button>
+        <button>
+          <img src={shareButton} alt="" />
+        </button>
       </section>
     </>
   );

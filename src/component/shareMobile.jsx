498040fe13d476ae053d398b858df860ea1c0b0a
Othniel <chukchi2345@gmail.com>
2025-03-14 20:47:49 +0100

    Modified mobile view, added animation

--- src/component/shareMobile.jsx
+++ src/component/shareMobile.jsx
@@ -2,11 +2,16 @@ import facebook from "../assets/icon-facebook.svg";
 import pinterest from "../assets/icon-pinterest.svg";
 import shareButton from "../assets/icon-share.svg";
 import twitter from "../assets/icon-twitter.svg";
+import { motion } from "framer-motion";
 
 const ShareComponentMobile = ({ changeValue }) => {
   return (
     <>
-      <section className="flex justify-around items-center shareComponent w-[100%] h-24 py-3">
+      <motion.section
+        animate={{ y: [500, 0] }}
+        transition={{ duration: 0.5 }}
+        className="flex justify-around items-center shareComponent w-[100%] h-24 py-3"
+      >
         <div className="flex items-center justify-around w-[60%] ">
           <button>
             <p className=" tracking-[0.4em] text-[0.7rem] font-light text-[#fff] ">
@@ -25,7 +30,9 @@ const ShareComponentMobile = ({ changeValue }) => {
         </div>
         <button
           className="shareButtonMobile flex justify-center items-center rounded-full w-[1.7rem] h-[1.7rem]"
-          onClick={() => {changeValue()}}
+          onClick={() => {
+            changeValue();
+          }}
         >
           <img
             className="w-[1rem] relative bottom-[0.1rem]"
@@ -33,7 +40,7 @@ const ShareComponentMobile = ({ changeValue }) => {
             alt=""
           />
         </button>
-      </section>
+      </motion.section>
     </>
   );
 };
